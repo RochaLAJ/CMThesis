0e1b5b4f61365e5a7f16dcd1576dc5c034b0e71b
RochaLAJ <43121804+RochaLAJ@users.noreply.github.com>
2021-08-18 01:30:19 -0300

    Create venn.R

---
+++ Tecidos/venn.R
@@ -0,0 +1,22 @@
+require(VennDiagram)
+
+data <- read.csv(file.choose(), header=T)
+
+venn.diagram(
+  x = list(data$mdt, data$lc),
+  category.names = c("MDT Tecidos" , "LC Tecidos"),
+  filename = '#14_venn_diagramm.png',
+  height=4500, width=4500,
+  print.mode=c('raw','percent'),
+  hyper.test=TRUE,
+  total.population = 86,
+  sub = 'n = 86',
+  sub.cex=2.5,
+  cat.cex=2.5,
+  cat.pos=2,
+  col=4:5,
+  fill=3:4,
+  alpha=0.3,
+  cex=2,
+  output=TRUE
+)
