06ad48602357d861866f64c2a275763c0924d621
RochaLAJ <43121804+RochaLAJ@users.noreply.github.com>
2022-10-26 19:36:14 +0000

    Update STAR.sh

--- Aligner/STAR.sh
+++ Aligner/STAR.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 STAR --runMode genomeGenerate --genomeDir Genome genomeFastaFiles /star_index/ref.fa  \ 
-         --genomeSAsparseD 2 --genomeSAindexNbases 2 --runThreadN 16 --genomeChrBinNbits 6
+         --genomeSAsparseD 2 --genomeSAindexNbases 2 --runThreadN 16 --genomeChrBinNbits 6 --limitGenomeGenerateRAM 20000000000
 
 STAR --runMode alignReads --outSAMtype BAM Unsorted SortedByCoordinte --runThreadN 16 --genomeDir Genome --outFilterMultimapNmax 50 
        \ --readFilesIn /chip/bam49.fasta --quantMode GeneCounts
