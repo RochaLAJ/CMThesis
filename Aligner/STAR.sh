942f13ad1bb2b63d66722f8354739cdb123686c1
RochaLAJ <rocha.luiz.aj@gmail.com>
2022-11-09 17:04:31 -0300

    Fixing parameters and styling

--- Aligner/STAR.sh
+++ Aligner/STAR.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
+$STAR --runMode genomeGenerate \
+--genomeDir ./ref/ \
+--genomeFastaFiles ./gencode/GRCh38.primary_assembly.genome.fa \
+--sjdbGTFfile ./gencode/gencode.v42.primary_assembly.annotation.gtf \
+--genomeSAsparseD 2 \
+--genomeSAindexNbases 2 \ 
+--runThreadN 16 \
+--genomeChrBinNbits 6  \
+--limitGenomeGenerateRAM 20000000000
 
-STAR --runMode genomeGenerate --genomeDir Genome genomeFastaFiles /star_index/ref.fa  \ 
-         --genomeSAsparseD 2 --genomeSAindexNbases 2 --runThreadN 16 --genomeChrBinNbits 6 --limitGenomeGenerateRAM 20000000000
 
-STAR --runMode alignReads --outSAMtype BAM Unsorted SortedByCoordinte --runThreadN 16 --genomeDir Genome --outFilterMultimapNmax 50 
-       \ --readFilesIn /chip/bam49.fasta --quantMode GeneCounts
-       \  TranscriptomeSAM --outTmpKeep /run/media/lemt/Backup/bam49 --outReadsUnmapped Fastx
+$STAR --runMode alignReads \
+--outSAMtype BAM Unsorted SortedByCoordinate \
+--runThreadN 16 \
+--genomeDir ./ref/ \
+--outFilterMultimapNmax 50 \
+--readFilesIn read1.fastq \
+--quantMode GeneCounts TranscriptomeSAM \
+--outReadsUnmapped Fastx
